9e4c580522087af956d6a6530c38d51c9ae0bb76
PhistucK <phistuck@gmail.com>
2017-06-05 20:51:05 +0300

    Added a resource-intensive filter support.

--- read_handler.py
+++ read_handler.py
@@ -1,10 +1,36 @@
 import webapp2, logging
 from database import get_feed_source_by_name
 
+# TODO - combine and move this duplication to utils.
+def get_feed_dom(feed):
+ from xml.dom import minidom
+ return minidom.parseString(feed.encode('utf-8'))
+
+def get_node_text(node):
+ if node.nodeType == 3:
+  return node.nodeValue
+ if node.nodeType == 1:
+  text = ''
+  for childNode in node.childNodes:
+   text += get_node_text(childNode)
+  return text
+ return ''
+
+def get_filtered_feed(feed, filter):
+ exact_filter = filter[6:]
+ feed_dom = get_feed_dom(feed)
+ entries = feed_dom.getElementsByTagName('entry')
+ for entry in entries:
+  title = unicode.lower(get_node_text(entry.getElementsByTagName('title')[0]))
+  if not exact_filter in title:
+   entry.parentNode.removeChild(entry)
+ return feed_dom.toxml()
+
 class ReadHandler(webapp2.RequestHandler):
  def get(self):
   from database import FeedSource
   name = self.request.get('feed')
+  filter = self.request.get('filter')
   source = get_feed_source_by_name(name)
   if not source:
    return self.error(404)
@@ -14,6 +40,20 @@ class ReadHandler(webapp2.RequestHandler):
   if not feed:
    # No backup is available.
    feed = source.feed.get()
+  if not feed:
+   self.error(503)
+   self.response.write('Error! The feed was not acquired yet.')
+   return
   if feed:
+   xml = feed.xml
    self.response.headers["Content-Type"] = "text/xml"
-   self.response.write(feed.xml)
+   if filter:
+    # Some day, perhaps add regular expression support.
+    if not unicode.startswith(filter, 'exact:'):
+      self.error(501)
+      self.response.write( \
+       'Error! Unsupported filter value. ' + \
+       'Only exact filters (filter=exact:...) are supported at the moment.')
+      return
+    xml = get_filtered_feed(xml, filter)
+   self.response.write(xml)
